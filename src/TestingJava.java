1220e1b230ac9359cf20499aaa3a33a42598793b
Albert <albertong85@gmail.com>
2023-07-27 14:34:31 -0400

    added changes to file

---
+++ src/TestingJava.java
@@ -0,0 +1,18 @@
+
+public class TestingJava {
+
+	
+	public static void main(String[] args) {
+		int x = 12;
+		System.out.println(x);
+		
+		System.out.println("Braised Pork Noodles are" + " $" + x);
+		
+		
+	String [] price = new String[3]; 
+		price [0] = "$15";
+		price [1] = "$21";
+		price [2] = "$34";
+	}
+
+}
